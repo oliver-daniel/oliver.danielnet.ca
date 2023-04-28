6c1703e52e221c9ee7ba4e021763333e9f32eae7
Oliver Daniel <oliverdaniel00@gmail.com>
2023-04-28 18:45:21 -0400

    initial scaffolding for redesign

---
+++ config/next-seo.config.js
@@ -0,0 +1,7 @@
+const config = {
+  blog: "https://oliverpdaniel.tumblr.com",
+  github: "https://github.com/oliver-daniel",
+  gitlab: "https://gitlab.com/oliver-daniel",
+  linkedin: "https://linkedin.com/in/oliverpdaniel",
+};
+export default config;
